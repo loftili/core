e771b209beaab219f3acc69e50ee3f32f9d2683e
Danny Hadley <dadley@sizethreestudios.com>
2014-06-02 15:26:07 -0400

    using defines for version number

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,8 @@
 #include <server.h>
 
 int main(int argc, char ** argv) {
-  return loftili::Server::run();
+  if(argc > 1)
+    std::cout << LOFTILI_PKG_VERSION << std::endl;
+  else
+    return loftili::Server::run();
 }
