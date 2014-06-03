a01518116155daf150ef7e90b19540dd81efd5a7
Danny Hadley <dadley@sizethreestudios.com>
2014-06-02 22:26:17 -0400

    working portaudio stream implementation

--- inc/audio/player.h
+++ inc/audio/player.h
@@ -1,6 +1,15 @@
 #ifndef _LOFTILI_PLAYER_H
 #define _LOFTILI_PLAYER_H
 
+#define NUM_SECONDS 5
+#define TABLE_SIZE 200
+#define SAMPLE_RATE 44100
+#define FRAMES_PER_BUFFER 64
+#ifndef M_PI
+#define M_PI 3.14159265
+#endif
+
+#include <math.h>
 #include <portaudio.h>
 #include "util/logger.h"
 
@@ -18,10 +27,28 @@ public:
 protected:
   std::string logName() { return "AudioPlayer"; }
 
+// static
+private:
+  static int onPlay(const void* input, void* output, unsigned long fpb, const PaStreamCallbackTimeInfo* time_info, PaStreamCallbackFlags flags, void* data);
+  static void onFinish(void* data);
+
+// instance
+private:
+  int whilePlaying(const void* input, void* output, unsigned long fpb, const PaStreamCallbackTimeInfo* time_info, PaStreamCallbackFlags flags);
+  int onFinished();
+
 private:
   Logger* log;
-  char* data;
   bool playing;
+  PaStream* stream;
+  PaStreamParameters output_config;
+  PaError last_error;
+  const PaDeviceInfo* device_info;
+  float sine[TABLE_SIZE];
+  int left_phase;
+  int right_phase;
+  char message[20];
+
 
 };
 
--- src/audio/player.cpp
+++ src/audio/player.cpp
@@ -2,7 +2,16 @@
 
 namespace loftili {
 
-AudioPlayer::AudioPlayer() : playing(false) {
+int AudioPlayer::onPlay(const void* input, void* output, unsigned long fpb, const PaStreamCallbackTimeInfo* time_info, PaStreamCallbackFlags flags, void* data) {
+  return ((AudioPlayer*)data)->whilePlaying(input, output, fpb, time_info, flags);
+}
+
+void AudioPlayer::onFinish(void* data) {
+  return;
+}
+
+
+AudioPlayer::AudioPlayer() : playing(false), stream(0), left_phase(0), device_info(0), right_phase(0), last_error(0) {
   log = new Logger(this);
   int success = Pa_Initialize();
 
@@ -10,6 +19,28 @@ AudioPlayer::AudioPlayer() : playing(false) {
     log->fatal("Unable to load portaudio library");
   else
     log->info("Successfully loaded portaudio library");
+
+  output_config.device = Pa_GetDefaultOutputDevice();
+  if(output_config.device == paNoDevice)
+    log->fatal("Failed to get default output device");
+  else
+    log->info("Found default output device");
+
+  device_info = Pa_GetDeviceInfo(output_config.device);
+  if(device_info != 0) {
+    log->info("Device name: ");
+    log->info(device_info->name);
+  }
+
+  output_config.channelCount = 2;
+  output_config.sampleFormat = paFloat32;
+  output_config.suggestedLatency = device_info->defaultLowOutputLatency;
+  output_config.hostApiSpecificStreamInfo = NULL;
+
+  for(int i=0; i<TABLE_SIZE; i++ ) {
+    sine[i] = (float) sin(((double)i/(double)TABLE_SIZE) * M_PI * 2.0);
+  }
+
 }
 
 AudioPlayer::~AudioPlayer() {
@@ -17,7 +48,55 @@ AudioPlayer::~AudioPlayer() {
   delete log;
 }
 
+int AudioPlayer::whilePlaying(const void* input, void* output, unsigned long fpb, const PaStreamCallbackTimeInfo* time_info, PaStreamCallbackFlags flags) {
+  float *out = (float*)output;
+  unsigned long i;
+
+  for(i=0; i < fpb; i++) {
+    *out++ = sine[left_phase];
+    *out++ = sine[right_phase];
+
+    left_phase += 1;
+    if(left_phase >= TABLE_SIZE)
+      left_phase -= TABLE_SIZE;
+
+    right_phase += 3;
+
+    if( right_phase >= TABLE_SIZE )
+      right_phase -= TABLE_SIZE;
+  }
+
+  return paContinue; 
+}
+
+int AudioPlayer::onFinished() {
+  return 1;
+}
+
 void AudioPlayer::start() {
+  if(playing)
+    return;
+
+  unsigned long fpb = paFramesPerBufferUnspecified;
+  last_error = Pa_OpenStream(&stream, NULL, &output_config, SAMPLE_RATE, fpb, paClipOff, &AudioPlayer::onPlay, this);
+
+  if (last_error != paNoError) {
+    log->fatal("UNABLE TO OPEN STREAM");
+    return;
+  }
+
+  last_error = Pa_SetStreamFinishedCallback(stream, &AudioPlayer::onFinish);
+
+  if(last_error != paNoError) {
+    Pa_CloseStream(stream);
+    log->fatal("UNABLE TO CLOSE STREAM");
+  }
+
+  last_error = Pa_StartStream(stream);
+  if(last_error != paNoError) {
+    log->fatal("NOT ABLE TO OPEN");
+  }
+
   playing = true;
 }
 
@@ -27,6 +106,9 @@ bool AudioPlayer::isPlaying() {
 
 void AudioPlayer::stop() {
   playing = false;
+  Pa_CloseStream(stream);
+  stream = 0;
+  log->info("Closing stream");
 }
 
 }
