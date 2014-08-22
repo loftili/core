57b6820c38f823996009180debc829243a67214d
Danny Hadley <dadley@sizethreestudios.com>
2014-08-21 22:48:47 -0400

    removing commented line from logger class

--- src/util/logger.cpp
+++ src/util/logger.cpp
@@ -26,7 +26,6 @@ void Logger::send(std::string msg, std::string level) {
   std::string time_str = ctime(&timer);
   std::string cleaned = time_str.substr(0, time_str.length() -1);
   printf("[%s] [%s] [%s]: %s \n", name.c_str(), level.c_str(), cleaned.c_str(), msg.c_str());
-  //std::cout<< "[" << name << "][" << level << "][" << cleaned << "] - " << msg << std::endl;
 }
 
 }
