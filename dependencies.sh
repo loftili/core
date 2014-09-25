4928b1a1b91304ec26c111f5a018077507eb0b25
Danny Hadley <dadley@sizethreestudios.com>
2014-09-24 21:29:13 -0400

    [LFTILI-62] updating download url for rapidjson

--- dependencies.sh
+++ dependencies.sh
@@ -4,7 +4,7 @@ function uninstall {
 
 function install {
   mkdir -p ./vendor/rapidjson
-  curl -o rapidjson.tar.gz http://ftp.sizethreestudios.com/artifacts/lofti.li/libs/rapidjson.tar.gz
+  curl -o rapidjson.tar.gz http://ftp.sizethreestudios.com/artifacts/loftili/libs/rapidjson.tar.gz
   tar xvzf rapidjson.tar.gz -C ./vendor/rapidjson
 }
 
