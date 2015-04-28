6162dfac5be7329e06b66e3c57289363a88ec3ae
Danny Hadley <dadley@sizethreestudios.com>
2015-04-28 16:32:48 -0400

    [LFTCE-4] using have_audio definition rather than breaking on device inability to play audio

--- load.sh
+++
@@ -1,9 +0,0 @@
-function req {
-  curl -o result.log --progress-bar --header 'x-loftili-auth: dadleyy' http://localhost:8888/stop
-  curl -o result.log --progress-bar --header 'x-loftili-auth: dadleyy' http://localhost:8888/start
-}
-
-for (( c=1; c<=50; c++ ))
-do
-  req c
-done
--- src/main.cpp
+++ src/main.cpp
@@ -96,3 +96,45 @@ int main(int argc, char* argv[]) {
   std::unique_ptr<loftili::Engine> p1(new loftili::Engine);
   return p1->Run();
 }
+
+/*
+template <class TT_H, class TT_T>
+class T_Typelist {
+  typedef TT_H T_Head;
+  typedef TT_T T_Tail;
+};
+
+class NullType { };
+
+template <class TT_H1=NullType, class TT_H2=NullType, class TT_H3=NullType> struct T_TypelistCons;
+
+template <class TT_H1>
+struct T_TypelistCons<TT_H1, NullType, NullType> {
+  typedef T_Typelist<TT_H1, NullType> T_Type;
+};
+
+template <class TT_H1, class TT_H2>
+struct T_TypelistCons<TT_H1, TT_H2, NullType> {
+  typedef T_Typelist<TT_H1, T_Typelist<TT_H2, NullType> > T_Type;
+};
+
+template <class TT_H1, class TT_H2, class TT_H3>
+struct T_TypelistCons {
+  typedef T_Typelist<TT_H1, T_Typelist<TT_H2, T_Typelist<TT_H3, NullType> > > T_Type;
+};
+
+
+class Audio { };
+class Video { };
+
+typedef typename T_TypelistCons<
+  #ifdef HAVE_AUDIO
+  Audio
+  #endif
+  #ifdef HAVE_VIDEO
+  Video
+  #endif
+>::T_Type Components;
+*/
+
+
